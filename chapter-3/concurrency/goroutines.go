87d91bb0dfd7ca026b535aabee3d114572588324
hoangnvhybrid <hoangnv@hybrid-technologies.vn>
2020-05-22 11:03:37 +0700

    su dung goroutines voi waitGroup

--- chapter-3/concurrency/goroutines.go
+++ chapter-3/concurrency/goroutines.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
+func sumArray(myArr []int, n int, wg *sync.WaitGroup) int {
+	defer wg.Done()
+	fmt.Println("Goroutines: ", n)
+	sum := 0
+	for _, value := range myArr {
+		sum += value
+	}
+
+	fmt.Println("sum: ", sum)
+	return sum
+}
 func main() {
-	fmt.Println("Hello world!")
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	myArr1 := []int{2, 3}
+	myArr2 := []int{7, 8}
+	total := 0
+	go func() {
+		total += sumArray(myArr1, 1, &wg)
+	}()
+	go func() {
+		total += sumArray(myArr2, 2, &wg)
+	}()
+	//time.Sleep(2 * time.Millisecond)
+	wg.Wait()
+	fmt.Println("total: ", total)
 }
